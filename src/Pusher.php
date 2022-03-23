210c397e7c56c4dae93df886253911a29f925ab2
angelo <angelo.maiorano@gmail.com>
2022-03-23 11:17:27 +0100

    debug...

--- src/Pusher.php
+++ src/Pusher.php
@@ -529,8 +529,6 @@ class Pusher implements LoggerAwareInterface, PusherInterface
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_value);
 
-        dd($ch);
-
         $response = $this->exec_curl($ch);
 
         dd($response);
